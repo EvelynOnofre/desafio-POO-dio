5bdc75c317db9502f23ddc14446b310e867840e2
EvelynOnofre <markmoraisonofre@gmail.com>
2022-08-05 22:28:03 -0300

    Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,45 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    private String titulo;
+    private String descrição;
+    private int cargaHoraria;
+    private LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
